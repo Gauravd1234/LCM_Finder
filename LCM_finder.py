339f3c2c269a3cf9f9137c8dbda06f736c75327c
Gauravd1234 <52632861+Gauravd1234@users.noreply.github.com>
2020-03-06 21:39:24 -0800

    Add files via upload

---
+++ LCM_finder.py
@@ -0,0 +1,13 @@
+def LCM(num1, num2):
+    firstnum = num1
+    secondnum = num2
+    while True:
+        if num1 < num2:
+            num1 += firstnum
+        if num2 < num1:
+            num2 += secondnum
+        if num1 == num2:
+            return "LCM: " + str(num1)
+
+
+#print(LCM(24, 13))
